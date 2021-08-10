8da6b9ee5a3af74b9209433b2d5143e458e72640
Moddy Pikiso <moddypikiso17@gmail.com>
2021-08-10 22:31:42 +0200

    app only capture data and displays in the greeted path

--- greetings-webapp.js
+++ greetings-webapp.js
@@ -4,90 +4,152 @@ module.exports = function greetings() {
     //recordNames
     //countsNames
     //No duplicates
-    
+
     let names;
     let count = 0;
     let listNames = [];
+    let check = [];
 
-
-    function greet(action, names) {
-
-        if (action === "English") {
-            return "Hello, " + names;
-        }
-
-        if (action === "IsiXhosa") {
-            return "Molo, " + names;
-        }
-
-        if (action === "Swahili") {
-            return "Jambo, " + names;
-        }
-    }
 
     function recordNames(action) {
 
-        if (action === "English") {
-            count += names;
-        }
+        var obj = {};
 
-        if (action === "IsiXhosa") {
-            count += names;
-        }
+        function setName() {
 
-        if (action === "Swahili") {
-            count += names;
-        }
-
-        listNames.push({
-            Name: names,
-            count,
-        });
-
-    }
-
-
-    function pushNames(names) {
-
-        if (!listNames.includes(names)) {
-
-            listNames.push(names);
-        }
-    }
-
-
-    function greetedNames() {
-        return listNames;
-    }
-
-    function getCount() {
-        return listNames.length;
-    }
-
-    function actionsFor(Name) {
-        const filteredNames = [];
-
-        // loop through all the entries in the action list 
-        for (let index = 0; index < listNames.length; index++) {
-            const action = listNames[index];
-            // check this is the type we are doing the total for 
-            if (action.Name === Name) {
-                // add the action to the list
-                filteredNames.push(action);
+            if (action.name) {
+                obj["name"] = action.name;
+                // obj["count"] = 1;
             }
         }
 
-        return filteredNames;
+        setName()
 
-        // return actionList.filter((action) => action.type === type);
+        function setLanguage() {
+
+
+            if (action.languageBtn === "English") {
+                obj["languageBtn"] = "Hello, "
+            }
+
+            if (action.languageBtn === "IsiXhosa") {
+                obj["languageBtn"] = "Molo, "
+
+            }
+
+            if (action.languageBtn === "Swahili") {
+                obj["languageBtn"] = "Jambo, "
+            }
+
+
+
+        }
+
+        setLanguage()
+
+
+        listNames.push(obj);
+        // console.log(listNames);
+        console.log(obj);
+
+
+
     }
 
+    function getCount(name) {
+
+        const counter = 0;
+        const recordList = [];
+
+        for (let index = 0; index < listNames.length; index++) {
+            const element = listNames[index];
+
+            if (element.name == name) {
+                recordList.push(element)
+                counter;
+            }
+
+        }
+        return { count: recordList.length, name }
+    }
+
+
+
+
+
+    // function recordNames(action) {
+
+    //     if (action === "English") {
+    //         count += names;
+    //     }
+
+    //     if (action === "IsiXhosa") {
+    //         count += names;
+    //     }
+
+    //     if (action === "Swahili") {
+    //         count += names;
+    //     }
+
+
+
+    // }
+
+
+    // function pushNames(names) {
+
+    //     if (!listNames.includes(names)) {
+
+    //         listNames.push(names);
+    //     }
+    // }
+
+
+    function greetedNames() {
+
+        //check empty object if has the index of names
+
+        for (let i = 0; i < listNames.length; i++) {
+
+            var names = listNames[i].name
+            if (check.indexOf(names) !== -1) {
+
+            } else {
+                check.push(names)
+            }
+        }
+        return check;
+    }
+
+
+
+
+    // function actionsFor(name) {
+    //     const filteredNames = [];
+    //     const counter = 0;
+    //     // loop through all the entries in the action list 
+    //     for (let index = 0; index < listNames.length; index++) {
+    //         const action = listNames[index].name;
+    //         console.log(listNames)
+    //         // check this is the type we are doing the total for 
+    //         if (action == name ) {
+    //             // add the action to the list
+    //             filteredNames.push(action);
+    //         }
+    //     }
+    //     // console.log(filteredNames)
+    //     return  {count: counter, name}
+
+
+    //     // return actionList.filter((action) => action.type === type);
+    // }
+
     return {
-        greet,
+
         recordNames,
         greetedNames,
-        actionsFor,
+        // actionsFor,
         getCount,
-        pushNames
+
     }
 }
--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 let express = require('express');
 // const flash = require('express-flash');
 // const session = require('express-session');
-let webApp = express();
+let app = express();
 
 //handlebars
 const exphbs = require('express-handlebars');
 
 const handlebarSetup = exphbs({
-    partialsDir: "./views/partials",
-    viewPath: './views',
-    layoutsDir: './views/layouts'
+  partialsDir: "./views/partials",
+  viewPath: './views',
+  layoutsDir: './views/layouts'
 });
 
 //html forms
@@ -19,90 +19,147 @@ const bodyParser = require('body-parser');
 
 //helpers
 const helpers = require('handlebars-helpers')();
+const session = require('express-session');
+
+const cookieParser = require('cookie-parser');
+
 
 // initialise the flash middleware
 // webApp.use(flash());
 
 
 //make the style in ccs folder visible
-webApp.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 // parse application/x-www-form-urlencoded
-webApp.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-webApp.use(bodyParser.json());
+app.use(bodyParser.json());
+app.use(cookieParser('secret'));
 
-webApp.engine('handlebars', handlebarSetup);
-webApp.set('view engine', 'handlebars');
+app.use(session({
+  cookie: { maxAge: null },
+  secret: "<add a secret string here>",
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use((req, res, next) => {
+  res.locals.message = req.session.message
+
+  delete req.session.message
+  next()
+});
 
 
-const greetings = require('./greetings-app');
+
+app.engine('handlebars', handlebarSetup);
+app.set('view engine', 'handlebars');
+
+
+const greetings = require('./greetings-webapp');
 
 const greeting = greetings();
 
-let counter = 0;
 
 //home route
-webApp.get('/', function (req, res) {
+app.get('/', function (req, res) {
 
-
-    //to keep data on the home route
-    res.render('index', {
-        recordNames: greeting.recordNames(),
-
-
-
-    });
-
+  //to keep data on the home route
+  res.render('index')
 })
 
-//greetings route
-webApp.post('/greeted', function (req, res) {
+// app.post('/', function(req,res){
 
-    let names = req.body.name;
+//   var greet = req.body.name
 
-    greeting.greet(names)
-    res.redirect('/')
+//   res.render({
+//       `{{greet}}`
+//   })
+
+// }
+
+
+// app.post('/', function(req,res){
+
+//   //check empty field and send a flash message for that.
+//   if(req.body.name == '' && req.body.languageBtn == null ){
+
+//       req.session.message = {
+//           type: 'ERROR!',
+//           intro: 'Empty field and no language',
+//           message: 'Please enter a name & select a language'
+//       }
+//       res.redirect('/');
+//   }
+//   if (req.body.name == ''){
+
+//       req.session.message = {
+//           type: 'ERROR!',
+//           intro: 'Empty field',
+//           message: 'Please enter a name'
+//       }
+//       res.redirect('/');
+//   }
+//   // else if (req.body.languageBtn == null ){
+
+//   //     req.session.message = {
+//   //         type: 'ERROR!',
+//   //         intro: 'no language',
+//   //         message: 'Please select a language'
+//   //     }
+//   //     res.redirect('/');
+//   // }
+//   else{
+//       req.session.message = {
+//           type: 'Success!',
+//           intro: 'Name entered',
+//           message: 'Name greeted sucessfully'
+//       }
+//       res.redirect('/');
+//   }
+
+// })
+
+
+// app.get('/greeted', function (req, res) {
+
+//   //to keep data on the home route
+//   res.render('/')
+// })
+
+app.post('/greeted', function (req, res) {
+
+  greeting.recordNames(req.body);
+  // console.log(req.body);
+
+  res.redirect('/');
 })
 
-//counter
-webApp.post('/counter', function (req, res) {
+// app.post('/language', function (req, res) {
 
-    greeting.recordNames(req.body.actionType)
-
-    res.redirect('/')
-
-})
-
-// initialise session middleware - flash-express depends on it
-// webApp.use(session({
-//     secret: "<add a secret string here>",
-//     resave: false,
-//     saveUninitialized: true
-// }));
-
-// //display warning messages using a flash message
-// webApp.get('/addFlash', function (req, res) {
-//     req.flash('info', 'Flash Message Added');
-//     res.redirect('/');
+//   greeting.recordNames(req.body.languageBtn)
+//   res.redirect('/');
 // });
 
-webApp.get('/greetedNames', function (req, res) {
+app.get('/greetedNames', function (req, res) {
 
-    res.render('greetedNames', { greetedNames: greeting.greetedNames()() });
+  res.render('greetedNames', { greetedNames: greeting.greetedNames() })
 
 });
 
-webApp.get('/greetedNames/:actiontype', function (req, res) {
-    const actionType = req.params.actiontype;
-    res.render('greetedNames', { greetedNames: greeting.actionsFor(actionType) });
-  });
-  
+app.get('/greetedNames/:name', function (req, res) {
+  const actionType = req.params.name;
+  res.render('count', { greetedNames: greeting.getCount(actionType) });
+});
+
+
+
 
 //start the server
-let PORT = process.env.PORT || 3007;
+let PORT = process.env.PORT || 3000;
 
-webApp.listen(PORT, function () {
-    console.log('App starting at port:', PORT);
+app.listen(PORT, function () {
+  console.log('App starting at port:', PORT);
 });
