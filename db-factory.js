7cfa6d7536e689d1f0bf291f124dc3255547a280
Moddy Pikiso <moddypikiso17@gmail.com>
2021-08-28 01:52:25 +0200

    try-catch db function

--- db-factory.js
+++ db-factory.js
@@ -7,55 +7,94 @@ module.exports = function greeted(pool) {
     function dbLog(name, languageBtn) {
 
         async function setDataToDb() {
-            await pool.query("INSERT INTO users (name,count,language) VALUES($1, $2, $3)", [name, 1, languageBtn])
+            try {
+                await pool.query("INSERT INTO users (name,count,language) VALUES($1, $2, $3)", [name, 1, languageBtn])
+
+            } catch (e) {
+
+                console.log('Catch an error: ', e)
+
+
+            }
 
         }
 
 
         async function getValueFromDb() {
-            var getting = await pool.query("SELECT DISTINCT name FROM users")
-            const arg = getting.rows
-            arg.forEach(element => {
-                obj.count++
-            })
 
-            //Promise
-            var listArray = await pool.query("SELECT * FROM users")
-            const array = listArray.rows
+            try {
+                var getting = await pool.query("SELECT DISTINCT name FROM users")
+                const arg = getting.rows
+                arg.forEach(element => {
+                    obj.count++
+                })
 
-            const database = array[array.length - 1]
+                //Promise
+                var listArray = await pool.query("SELECT * FROM users")
+                const array = listArray.rows
 
-            // console.log(database, obj.count )
-            return await { data: database, count: obj.count }
+                const database = array[array.length - 1]
 
-        }
+                // console.log(database, obj.count )
+                return await { data: database, count: obj.count }
 
-        return { setDataToDb, getValueFromDb }
+
+
+
+            } catch (e) {
+
+                console.log('Catch an error: ', e)
+
+
+            }
+
+        } return { setDataToDb, getValueFromDb }
 
     }
 
-    async function getGreetedList() {
-        var greetList = (await pool.query("SELECT DISTINCT name FROM users")).rows
 
-        const list = []
-        greetList.forEach(element => {
-            list.push(element.name)
-        })
-        return list 
+    async function getGreetedList() {
+
+        try {
+
+            var greetList = (await pool.query("SELECT DISTINCT name FROM users")).rows
+
+            const list = []
+            greetList.forEach(element => {
+                list.push(element.name)
+            })
+            return list
+
+        } catch (e) {
+            console.log('Catch an error: ', e)
+
+        }
+
 
     }
 
     async function getCountOFName(names) {
-        var nameCountNames = await pool.query("SELECT name FROM users")
-        const data = nameCountNames.rows
-        const listCount = []
-        data.forEach(element => {
-            if (element.name === names) {
-                listCount.push(element.name)
-            }
-        })
 
-        return { count: listCount.length, names }
+        try {
+
+            var nameCountNames = await pool.query("SELECT name FROM users")
+            const data = nameCountNames.rows
+            const listCount = []
+            data.forEach(element => {
+                if (element.name === names) {
+                    listCount.push(element.name)
+                }
+            })
+
+            return { count: listCount.length, names }
+
+
+        } catch (e) {
+
+            console.log('Catch an error: ', e)
+
+
+        }
 
         // setTimeout(() => {
         // res.render('count', { count: listCount.length, names })
@@ -66,7 +105,15 @@ module.exports = function greeted(pool) {
 
     async function reset() {
 
-        await pool.query("DELETE FROM users")
+        try {
+            await pool.query("DELETE FROM users")
+
+
+        } catch (e) {
+            console.log('Catch an error: ', e)
+
+        }
+
         // .then(resp => {
 
         //     res.redirect('/')
